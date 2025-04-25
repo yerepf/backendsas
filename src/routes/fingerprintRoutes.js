df944cae03c2d59950077b476ee071e08f0789f7
Yeremy Pujols <148657155+yerepf@users.noreply.github.com>
2025-04-25 14:47:32 -0400

    fix get to post

--- src/routes/fingerprintRoutes.js
+++ src/routes/fingerprintRoutes.js
@@ -12,8 +12,8 @@ router.post(
     biometricController.createOrUpdateTemplate
 );
 
-// GET /api/biometrics/student/:studentId - Obtener huella por estudiante
-router.get(
+// POST /api/biometrics/student/:studentId - Obtener huella por estudiante
+router.post(
     '/student/:studentId',
     protect,
     authorize('AdminInstitucion', 'Profesor', 'PersonalApoyo'), // Profesor también puede ver para asistencia
@@ -29,8 +29,8 @@ router.delete(
 );
 
 
-// GET /api/biometrics/template - Obtener ID de estudiante por datos de huella
-router.get(
+// POST /api/biometrics/template - Obtener ID de estudiante por datos de huella
+router.post(
     '/template',
     protect,
     authorize('AdminInstitucion', 'Profesor', 'PersonalApoyo'), // Profesor también puede ver para asistencia
